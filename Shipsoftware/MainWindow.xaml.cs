14619cf8c10655b1ac5c397e0e78ef88f3c275b9
Jori <jokkeri92@gmail.com>
2016-11-22 10:11:12 +0200

    Uusi yhteys + kompassi route

--- Shipsoftware/MainWindow.xaml.cs
+++ Shipsoftware/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,44 @@ namespace Shipsoftware
         public MainWindow()
         {
             InitializeComponent();
+      
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            string[] lines = System.IO.File.ReadAllLines(@"yhdistys.txt");
+            connetionString = "Data Source=" + lines[0] + ";  Initial Catalog=" + lines[1] + ";User ID=" + lines[2] + ";Password=" + lines[3]; // yhteyden tiedot
+            cnn = new SqlConnection(connetionString);
+
+            try   // yhteyden avaus yritys
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+            }
+            catch
+            {
+                MessageBox.Show("Can not open connection ! "); // yhteys virhe viesti
+            }
+
+            SqlDataReader Reader;
+            SqlCommand cmd = new SqlCommand("select ShipName from Ships", cnn);
+            try
+            {
+                Reader = cmd.ExecuteReader();
+            }
+            catch
+            {
+                MessageBox.Show("ei toimi");
+                return;
+            }
+            while (Reader.Read())
+            {
+                lstLaivat.Items.Add(Reader["ShipName"]);
+            }
+
+            cnn.Close();
         }
     }
 }
