428fd357f559ab3908cb46d2086839416d0ca4b2
Jori <jokkeri92@gmail.com>
2016-11-23 15:12:18 +0200

    kompassi,laivaID, Etsi nappi

--- Shipsoftware/MainWindow.xaml.cs
+++ Shipsoftware/MainWindow.xaml.cs
@@ -21,20 +21,15 @@ namespace Shipsoftware
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connetionString = null;
+        SqlConnection cnn;
+
         public MainWindow()
         {
             InitializeComponent();
-      
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            string connetionString = null;
-            SqlConnection cnn;
             string[] lines = System.IO.File.ReadAllLines(@"yhdistys.txt");
             connetionString = "Data Source=" + lines[0] + ";  Initial Catalog=" + lines[1] + ";User ID=" + lines[2] + ";Password=" + lines[3]; // yhteyden tiedot
             cnn = new SqlConnection(connetionString);
-
             try   // yhteyden avaus yritys
             {
                 cnn.Open();
@@ -44,24 +39,56 @@ namespace Shipsoftware
             {
                 MessageBox.Show("Can not open connection ! "); // yhteys virhe viesti
             }
-
+        }
+       
             SqlDataReader Reader;
-            SqlCommand cmd = new SqlCommand("select ShipName from Ships", cnn);
-            try
-            {
-                Reader = cmd.ExecuteReader();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+                
+                SqlCommand cmd = new SqlCommand("select ShipName, ShipID from Ships", cnn);
+                try
+                {
+                    Reader = cmd.ExecuteReader();
+                }
+                catch
+                {
+                    MessageBox.Show("ei toimi");
+                    return;
+                }
+                while (Reader.Read())
+                {
+                    //  lstLaivat.Items.Add(Reader["ShipName"]);
+
+                    ListBoxItem Item = new ListBoxItem();
+                    Item.Content = Reader["ShipName"];
+                    Item.Tag = Reader["ShipID"];
+                    lstLaivat.Items.Add(Item);
+                    Item.MouseLeftButtonUp += ListBoxItem_MouseLeftButtonUp;
+
+                
             }
-            catch
-            {
-                MessageBox.Show("ei toimi");
-                return;
-            }
-            while (Reader.Read())
-            {
-                lstLaivat.Items.Add(Reader["ShipName"]);
-            }
+            cnn.Close();
+        }
+        private void ListBoxItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            cnn.Open();
+
+            lblLaivaID.Content = ((ListBoxItem)lstLaivat.SelectedItem).Tag;
+            SqlCommand cmd = new SqlCommand("select Course from Ships WHERE ShipID=" + ((ListBoxItem)lstLaivat.SelectedItem).Tag, cnn);
+            Reader = cmd.ExecuteReader();
+            Reader.Read();
+            lblKompassiAste.Content = Reader["Course"];
+
+
 
             cnn.Close();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
